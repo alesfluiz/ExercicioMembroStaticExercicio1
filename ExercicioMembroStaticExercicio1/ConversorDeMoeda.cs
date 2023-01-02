fbcfb61924ac9c364d0d6a08ad9e093ce179a1d7
ALESF <ALESF@LAPTOP-7JNANN11>
2023-01-02 07:49:56 -0300

    Add project files.

---
+++ ExercicioMembroStaticExercicio1/ConversorDeMoeda.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace ExercicioMembroStaticExercicio1 {
+    internal class ConversorDeMoeda {
+
+        public static double Iof = 6.0;
+
+        public static double Real(double moedaD, double QmoedaD) {
+            double total = moedaD * QmoedaD;
+            return total = total + total * Iof / 100;
+        }
+
+
+
+    }
+}
---
+++ ExercicioMembroStaticExercicio1/Program.cs
@@ -0,0 +1,22 @@
+﻿using ExercicioMembroStaticExercicio1;
+using System;
+using System.Globalization;
+
+namespace ExercicioMembroStatico1
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Qual é a cotação do dólar? ");
+            double valorDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double compra = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double resultado = ConversorDeMoeda.Real(valorDolar, compra);
+
+            Console.WriteLine("Valor a ser pago em reais = " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
